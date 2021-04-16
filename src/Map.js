62a8cc8f1df7f5475ec78a93100ba9a0088222c8
mayu12525 <mayurichaitanya2901@gmail.com>
2021-04-16 15:40:43 +0530

    first commit

---
+++ src/Map.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { MapContainer as LeafletMap, TileLayer } from "react-leaflet";
+import "./Map.css";
+import { showCirclesOnMap } from "./util.js";
+function Map({ center, zoom, countries, caseType }) {
+  return (
+    <div className="map">
+      <LeafletMap center={center} zoom={zoom}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        ></TileLayer>
+        {countries.length && showCirclesOnMap(countries, caseType)}
+      </LeafletMap>
+    </div>
+  );
+}
+
+export default Map;
